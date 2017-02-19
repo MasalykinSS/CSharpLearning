3868ebab81d395a6b192d578d00eb62e8024115c
MasalykinSS <masalykin.ss@gmail.com>
2017-02-19 11:01:30 +0200

    gui added

---
+++ DZ2_with_GUI/Fight/Fight/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Fight
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 24;
+            this.listBox1.Location = new System.Drawing.Point(16, 203);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(603, 100);
+            this.listBox1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 45);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(607, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(16, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(603, 98);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Сделать ход";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 334);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Fight";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DZ2_with_GUI/Fight/Fight/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fight
+{
+
+    public partial class Form1 : Form
+    {
+
+        class Fighter
+        {
+            private int healthPoints;
+            private int blockValue;
+
+            public Fighter()
+            {
+                healthPoints = 20;
+            }
+
+            public void HeadPunch()
+            {
+                if (healthPoints < 10)
+                    healthPoints = 0;
+                else
+                    healthPoints -= 10;
+            }
+
+            public void BodyPunch()
+            {
+                if (healthPoints < 7)
+                    healthPoints = 0;
+                else
+                    healthPoints -= 7;
+            }
+
+            public void LegsPunch()
+            {
+                if (healthPoints < 5)
+                    healthPoints = 0;
+                else
+                    healthPoints -= 5;
+            }
+
+            public int getHealth()
+            {
+                return healthPoints;
+            }
+
+            public int getBlock()
+            {
+                return blockValue;
+            }
+
+            public void setBlock(int bv)
+            {
+                blockValue = bv;
+            }
+
+        }
+
+        Fighter player = new Fighter();
+        Fighter enemy = new Fighter();
+        System.Random rnd = new System.Random();
+        int move = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                    
+                    if (move == 1)
+                    {
+                        listBox1.Items.Clear();
+                        int punch;
+                        label1.Text="Ход противника. Выберите,какую часть тела заблокировать:";
+                        punch = comboBox1.SelectedIndex + 1;
+                        int block = rnd.Next(1, 3);
+                        enemy.setBlock(block);
+                        if (enemy.getBlock() == punch)
+                                listBox1.Items.Add("Противник заблокировал удар!");
+                        else
+                        {
+                            listBox1.Items.Add("Вы попали по противнику!");
+                            switch (punch)
+                            {
+                                case 1:
+                                    enemy.HeadPunch();
+                                    break;
+
+                                case 2:
+                                    enemy.BodyPunch();
+                                    break;
+
+                                case 3:
+                                    enemy.LegsPunch();
+                                    break;
+                            }
+                        }
+                        listBox1.Items.Add("Ваше здоровье:" + player.getHealth());
+                        listBox1.Items.Add("Здоровье противника:" + enemy.getHealth());
+                        move = 2;
+                    }
+                    else
+                    {
+                        listBox1.Items.Clear();
+                        int punch = rnd.Next(1, 3);
+                        label1.Text="Ваш ход. Выберите, куда ударить!";
+                        player.setBlock(comboBox1.SelectedIndex + 1);
+                        if (player.getBlock() == punch)
+                             listBox1.Items.Add("Вы заблокировали удар!");
+                        else
+                        {
+                            listBox1.Items.Add("Противник ударил вас!");
+                            switch (punch)
+                            {
+                                case 1:
+                                    player.HeadPunch();
+                                    break;
+
+                                case 2:
+                                    player.BodyPunch();
+                                    break;
+
+                                case 3:
+                                    player.LegsPunch();
+                                    break;
+                            }
+                        }
+                        listBox1.Items.Add("Ваше здоровье:" + player.getHealth());
+                        listBox1.Items.Add("Здоровье противника:" + enemy.getHealth());
+                        move = 1;
+                    }
+
+            if (enemy.getHealth() == 0)
+            {
+                listBox1.Items.Add("Поздравляем! Вы одолели противника!");
+                button1.Visible = false;
+            }
+            else if (player.getHealth() == 0)
+            {
+                listBox1.Items.Add("Вы проиграли. Противник одолел вас :-(");
+                button1.Visible = false;
+            }
+
+}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Первый ход за вами. Выберите, кудаударить!";
+            comboBox1.Items.Add("Голова");
+            comboBox1.Items.Add("Тело");
+            comboBox1.Items.Add("Ноги");
+        }
+    }
+}
